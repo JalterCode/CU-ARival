7cd677e0336067cc33fcec6670881c38ac14ab2c
ilyhus001 <isasi2013@outlook.com>
2025-02-02 01:32:41 -0500

    Automate button creation

--- My project (3)/Assets/Scripts/findRoomScript.cs
+++ My project (3)/Assets/Scripts/findRoomScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Collections.Generic;
+using System.Linq;
 
 public class findRoomScript : MonoBehaviour
 {
@@ -10,6 +12,11 @@ public class findRoomScript : MonoBehaviour
     public Button Button5111;
     public Button Button5105;
     public Button Button5101;
+
+    //Making this shi better
+    public SortedDictionary<string, Button> roomButtons = new SortedDictionary<string, Button>();
+    public RectTransform panel;
+    public Button buttonPrefab;
     //public TMP_Dropdown dropDown;
     private static string destination;
     private Button clickedButton; 
@@ -37,6 +44,63 @@ public class findRoomScript : MonoBehaviour
         
         
         
+    }
+
+
+    /*Note to self: potentially pass selected floor number to function and get all rooms thay start with floor number*/
+    public void GenerateButtons() { 
+        List<Transform> roomParents = GetFloorLocations();
+        List<string> roomNames = new List<string>();
+        foreach (Transform parent in roomParents)
+        {
+            foreach (Transform room in parent)
+            {
+                roomNames.Add(room.name+"Button"); 
+            }
+        }
+
+        foreach (Transform btn in panel)
+        {
+            Button button = btn.GetComponent<Button>();
+            roomButtons[btn.name] = button;
+        }
+
+
+        float Yoffset = -116f;
+        foreach (string roomName in roomNames)
+        {
+            if (!roomButtons.ContainsKey(roomName)) // If button does not exist, create it
+            {
+                Button newButton = Instantiate(buttonPrefab, panel);
+                newButton.name = roomName;
+                newButton.GetComponentInChildren<TMP_Text>().text = roomName;
+
+                Button lastButton = roomButtons.Values.Last();
+                RectTransform lastRect = lastButton.GetComponent<RectTransform>();
+                float newY = lastRect.anchoredPosition.y - Yoffset;
+
+                RectTransform rectTransform = newButton.GetComponent<RectTransform>();
+                Vector3 originalPosition = rectTransform.anchoredPosition;
+                rectTransform.anchoredPosition = new Vector3(originalPosition.x, newY, originalPosition.z);
+
+                
+                newButton.onClick.AddListener(() => OnNavigateButtonClicked(newButton));
+
+                roomButtons[roomName] = newButton;
+            }
+        }
+    }
+
+    public List<Transform> GetFloorLocations() {
+        List<Transform> locations = new List<Transform>();
+        foreach (Transform obj in FindObjectsOfType<Transform>())
+        {
+            if (obj.name.EndsWith("Locations"))
+            {
+                locations.Add(obj);
+            }
+        }
+        return locations;
     }
 
     public string GetRoom()
