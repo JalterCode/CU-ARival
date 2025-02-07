0622b97d9ae3166ee2123c108bddd75ac9b2945e
JalterCode <jakester502@gmail.com>
2025-02-06 22:47:55 -0500

    Update NavigationManager.cs

--- My project (3)/Assets/Scripts/NavigationManager.cs
+++ My project (3)/Assets/Scripts/NavigationManager.cs
@@ -91,6 +91,7 @@ public class NavigationManager : MonoBehaviour
         
             trackedImageManager.enabled = false;
             isScanningEnabled = false;
+            //s
 
         }
     }
