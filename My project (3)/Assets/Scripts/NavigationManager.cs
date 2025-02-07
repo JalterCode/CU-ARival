c65994327cca8ad0b00d29819561057ee2d72199
ilyhus001 <isasi2013@outlook.com>
2025-02-07 04:56:33 -0500

    Micro nap time. Commit for good measure to not lose work

--- My project (3)/Assets/Scripts/NavigationManager.cs
+++ My project (3)/Assets/Scripts/NavigationManager.cs
@@ -116,11 +116,7 @@ public class NavigationManager : MonoBehaviour
         GameObject roomObject = GameObject.Find(roomName);
 
 
-        if (roomObject.gameObject.name.StartsWith("6")) {
-            endPoint = GameObject.Find("stairs5").transform;
-            Debug.Log("Navigation room updated: " + roomName);
-        }
-        else if (roomObject != null)
+        if (roomObject != null)
         {
             endPoint = roomObject.transform;
             Debug.Log("Navigation room updated: " + roomName);
