9532658a6239f37c6d987c0529d6af7c2fed2d4a
firas <firaselezzi1@gmail.com>
2025-02-23 23:23:08 -0500

    Search Bar Phase 1
    None functional

---
+++ My project (3)/Assets/Scripts/SearchScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SearchScript : MonoBehaviour
+{
+    public TextMeshProUGUI inputField;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
