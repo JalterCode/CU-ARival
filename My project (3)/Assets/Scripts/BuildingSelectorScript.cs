bbb52e638ac2f5ef81718ce52a9839512e2557b2
JalterCode <jakester502@gmail.com>
2025-03-13 14:09:51 -0400

    uiherqfgerf

--- My project (3)/Assets/Scripts/BuildingSelectorScript.cs
+++ My project (3)/Assets/Scripts/BuildingSelectorScript.cs
@@ -5,19 +5,6 @@ public class MainMenu : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
-        if (!string.IsNullOrEmpty(sceneName)) 
-        {
-            SceneManager.LoadScene(sceneName);
-        }
-        else
-        {
-            Debug.LogError("Scene name is null or empty!");
-        }
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Quitting Game...");
-        Application.Quit();
+        SceneManager.LoadScene(sceneName);
     }
 }
