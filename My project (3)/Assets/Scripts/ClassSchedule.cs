526690586190f6ed86b34ce635213a7679bd7725
ilyhus001 <isasi2013@outlook.com>
2025-03-25 21:11:00 -0400

    Fixed days

--- My project (3)/Assets/Scripts/ClassSchedule.cs
+++ My project (3)/Assets/Scripts/ClassSchedule.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum Days {
-    Monday = 0,
-    Tuesday = 1,
-    Wednesday = 2,
-    Thursday = 3,
-    Friday = 4,
-    Saturday = 5,
-    Sunday = 6
+    Sunday = 0,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6
 }
 
 public class ClassSchedule : MonoBehaviour
